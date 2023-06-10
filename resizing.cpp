d141ba4535a5ad636656dd695b5a65b92d359cb5
Qianqiu1 <99669760+Qianqiu1@users.noreply.github.com>
2023-06-10 18:35:59 +0200

    Update resizing.cpp

--- resizing.cpp
+++ resizing.cpp
@@ -19,21 +19,13 @@ void resizing(String imageFilePathResizing)
     cin >> width;
     cout << "Enter the height value: ";
     cin >> height;
-    //int down_width = 300;
-    //int down_height = 200;
-    Mat resized_down;
-    // resize down
-    resize(image, resized_down, cv::Size(width, height), cv::INTER_LINEAR);
-    // let's upscale the image using new width and height
-    //int up_width = 600;
-    //int up_height = 400;
-    //Mat resized_up;
-    //// resize up
+    Mat resized;
+    resize(image, resized, cv::Size(width, height), cv::INTER_LINEAR);
     //resize(image, resized_up, cv::Size(up_width, up_height), cv::INTER_LINEAR);
     // Display Images and press any key to continue
     namedWindow("Resized Down by defining height and width", cv::WINDOW_NORMAL);
     imshow("Original Image", image);
-    imshow("Resized Down by defining height and width", resized_down);
+    imshow("Resized Down by defining height and width", resized);
     waitKey(0);
     //namedWindow("Resized Up image by defining height and width", cv::WINDOW_NORMAL);
     //cv::imshow("Resized Up image by defining height and width", resized_up);
